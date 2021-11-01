419e60b396628955001ecbb336d41f4d1eec3ab5
Brandon Spell <brandon.spell@rackspace.com>
2021-10-31 20:47:26 -0500

    update vpc parameters

--- vpc.tf/vpc.tf
+++ vpc.tf/vpc.tf
@@ -9,24 +9,28 @@ module "vpc" {
   private_subnets         = local.priv_networks
   public_subnets          = local.pub_networks
   enable_nat_gateway      = true
-  single_nat_gateway      = false
-  one_nat_gateway_per_az  = true
+  single_nat_gateway      = true
+  enable_dns_hostnames    = true  
 
   
   public_subnet_tags  = {
-    Name = "${var.namespace}-Public"
-    Environment = var.environment
+    Name                                              = "${var.namespace}-Public"
+    Environment                                       = var.environment
+    "kubernetes.io/cluster/${var.namespace}-K8Cluster"  = "shared"
+    "kubernetes.io/role/elb"                          = "1"  
   }
 private_subnet_tags = {
     Name = "${var.namespace}-Private"
     Environment = var.environment
+    "kubernetes.io/cluster/${var.namespace}-K8Cluster"  = "shared"
+    "kubernetes.io/role/internal-elb"                 = "1"
   }
   tags  = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment
   }
   vpc_tags = {
     Name = "${var.namespace}-VPC"
-    Environment = "dev"
+    Environment = var.environment
   }
 }
