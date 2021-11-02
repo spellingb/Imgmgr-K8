69947e9ef0fb2b41eb76b88f018b685041c7be3d
Brandon Spell <brandon.spell@rackspace.com>
2021-11-02 13:15:48 -0500

    add simple ps script to set kube config var

---
+++ setenv.ps1
@@ -0,0 +1,19 @@
+$here = (Get-Location).Path
+
+if ((Get-Command -Name 'git' -CommandType Application -ErrorAction SilentlyContinue).Count -gt 0) {
+    $gitTopLevel = & { git rev-parse --show-toplevel 2> $null }
+    if ($gitTopLevel.Length -ne 0) {
+        $gitBranch = (git branch | Where-Object { $_ -match "\*" }).Trimstart('* ')
+        if ( $gitBranch -match 'Master' ) {
+            $subfolder = 'common'
+        } else {
+            $subfolder = $gitBranch
+        }
+    }
+}
+$config = '{0}\config' -f $here, $subfolder
+if ( Test-Path $config ) {
+    $ENV:KUBECONFIG="{0};{1}" -f  $ENV:KUBECONFIG, $config
+}
+
+
