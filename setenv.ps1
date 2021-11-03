e4daff7e15ca2a42f9ba6e6360caf1e032feb07c
Brandon Spell <brandon.spell@rackspace.com>
2021-11-03 00:42:00 -0500

    update kubeconfig mapping

--- setenv.ps1
+++ setenv.ps1
@@ -1,19 +1,33 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    [ValidateSet('dev','prod')]
+    $environment
+)
 $here = (Get-Location).Path
-
-if ((Get-Command -Name 'git' -CommandType Application -ErrorAction SilentlyContinue).Count -gt 0) {
-    $gitTopLevel = & { git rev-parse --show-toplevel 2> $null }
-    if ($gitTopLevel.Length -ne 0) {
-        $gitBranch = (git branch | Where-Object { $_ -match "\*" }).Trimstart('* ')
-        if ( $gitBranch -match 'Master' ) {
-            $subfolder = 'common'
-        } else {
-            $subfolder = $gitBranch.TrimStart('ENV-')
+if ($environment){
+    $subfolder = $environment
+} else {
+    if ((Get-Command -Name 'git' -CommandType Application -ErrorAction SilentlyContinue).Count -gt 0) {
+        $gitTopLevel = & { git rev-parse --show-toplevel 2> $null }
+        if ($gitTopLevel.Length -ne 0) {
+            $gitBranch = (git branch | Where-Object { $_ -match "\*" }).Trimstart('* ')
+            if ( $gitBranch -match 'Master' ) {
+                $subfolder = 'common'
+            } else {
+                $subfolder = $gitBranch.TrimStart('ENV-')
+            }
         }
     }
 }
-$config = '{0}\config' -f $here, $subfolder
+$config = '{0}\.kube\{1}\kubeconfig_top-lab15-capstone-{1}' -f $here, $subfolder
 if ( Test-Path $config ) {
-    $ENV:KUBECONFIG="{0};{1}" -f  $ENV:KUBECONFIG, $config
+    if($ENV:KUBECONFIG){
+        $ENV:KUBECONFIG="{0};{1}" -f  $ENV:KUBECONFIG, $config
+    }else {
+        $ENV:KUBECONFIG= $config
+    }
 }
 
 
